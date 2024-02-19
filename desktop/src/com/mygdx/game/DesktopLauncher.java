6110ed4c410b060e8b12b66c42007c93dc2885a4
Earl John Castillo <ecastil3@uwo.ca>
2024-02-19 12:40:57 -0500

    Inititial Commit with LIBGDX lib

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MainGame;
 
 //Main Class To Launch The Program
 public class DesktopLauncher {
