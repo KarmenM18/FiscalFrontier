3043e787ca17342b9a4f9f76374546478d0c4759
Earl John Castillo <ecastil3@uwo.ca>
2024-02-19 12:32:21 -0500

    Inititial Commit with LIBGDX lib

---
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.game.MainGame;
+
+//Main Class To Launch The Program
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Navigating the Fiscal Frontier");
+		new Lwjgl3Application(new MainGame(), config);
+	}
+}
