5620c5e94941891a62dca3e7fba22e1aeb58b1ea
Kuan Yu Chen <kchen546@uwo.ca>
2024-03-27 14:26:33 -0400

    nothing

---
+++ core/test/com/mygdx/game/Node/NodeTest.java
@@ -0,0 +1,90 @@
+package com.mygdx.game.Node;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void activate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testActivate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReachable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGreen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGray() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNoColor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSprite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNorth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEast() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSouth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setWest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNorth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEast() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSouth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getXPos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getYPos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMapX() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMapY() {
+    }
+}
---
+++ core/test/com/mygdx/game/Node/StarNodeTest.java
@@ -0,0 +1,22 @@
+package com.mygdx.game.Node;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StarNodeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void activate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkStar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStarCost() {
+    }
+}
