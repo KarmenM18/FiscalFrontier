eb41975059c2fa10ee4586e1110eb9bf3d61543c
Karmen <karmenjot.minhas@gmail.com>
2024-04-01 21:37:22 -0400

    Added Javadocs documentation

--- core/src/com/mygdx/game/Items/Shield.java
+++ core/src/com/mygdx/game/Items/Shield.java
@@ -7,13 +7,19 @@ import com.mygdx.game.GameState;
 import com.mygdx.game.Player;
 import com.mygdx.game.SoundSystem;
 
+/**
+ * Creates a Shield Item to be added to the ShopScreen.
+ * @author Karmen Minhas (kminhas7)
+ * @author Kevin Chen (kchen546)
+ * @author Franck Limtung (flimtung)
+ */
+
 public class Shield extends Item{
     public Shield(Skin skin){
         super("Shield", false, skin);
         this.price = 150;
         this.description = "Immunity: protects player from next penalty.";
     }
-
     public boolean use(Player player, GameState gameState, Stage stage) {
         if (player.getHasShield()) {
             SoundSystem.getInstance().playSound("error.wav");
